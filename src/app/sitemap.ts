99955d79c9dc635a9af62143025eb054d423f72f
LeBibbb <ibiberisha02@gmail.com>
2026-03-13 18:48:53 +0100

    sitemap fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://rayanback.be'
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://www.rayan-back.be'
 
   return [
     {
